8558bbb7189f9e9005eea5ee63b9bacabca469a2
andregom <andre.oliveira@ccc.ufcg.edu.br>
2023-04-13 17:38:49 -0300

    navigates to apointment booking programmaticaly

--- src/app/components/appointment-form/appointment-form.component.ts
+++ src/app/components/appointment-form/appointment-form.component.ts
@@ -1,4 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -9,22 +11,40 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class AppointmentFormComponent implements OnInit {
   form: FormGroup;
+  now: Date = new Date();
+  oneHourFromNow: Date;
+  startDate: string;
+  finnishDate: string;
 
-  constructor () {
+  constructor(
+    private ativateRoute: ActivatedRoute,
+    public router: Router
+  ) {
+    this.oneHourFromNow = new Date();
+    this.oneHourFromNow.setHours(this.now.getHours() + 1)
+    this.startDate = formatDate(this.now, 'yyyy-MM-ddTHH:mm', 'en-US', '+0530');
+    this.finnishDate = formatDate(this.oneHourFromNow, 'yyyy-MM-ddTHH:mm', 'en-US', '+0530');
+    console.log(this.startDate);
     this.form = new FormGroup({
       title: new FormControl('New Appointment', Validators.required),
       participant: new FormControl(''),
       description: new FormControl(''),
-      start: new FormControl('', Validators.required),
-      finnish: new FormControl('', Validators.required),
+      start: new FormControl(`${this.startDate}`, Validators.required),
+      finnish: new FormControl(`${this.finnishDate}`, Validators.required),
     })
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(appointment: any) {
+  goToTimeSlots(partialAppointmanet: any) {
+    this.router.navigate([this.router.url, 'choose_available'])
+    console.log(partialAppointmanet);
+  }
 
+  onSubmit(appointment: any) {
+      this.router.navigate([this.router.url, 'details'])
+      console.log(appointment);
   }
 
 }
