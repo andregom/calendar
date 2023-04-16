0ccb04be4c11293a1cdcf667375096e61ae68499
andregom <andre.oliveira@ccc.ufcg.edu.br>
2023-04-16 04:43:52 -0300

    The closest I've got

--- src/app/directives/date-selectio-handler.directive.ts
+++ src/app/directives/date-selectio-handler.directive.ts
@@ -1,5 +1,5 @@
 import { AfterContentInit, AfterViewChecked, Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, Renderer2 } from '@angular/core';
-import { MatCalendar } from '@angular/material/datepicker';
+import { MatCalendar, MatCalendarCell } from '@angular/material/datepicker';
 import { BehaviorSubject } from 'rxjs';
 
 @Directive({
@@ -12,9 +12,10 @@ export class DateSelectioHandlerDirective implements OnInit, AfterViewChecked {
 
   constructor(
     private matCalendar: MatCalendar<Date>,
+    private elRef: ElementRef,
     private renderer: Renderer2
   ) {
-    this.selectedDates = new BehaviorSubject(new Set());
+    this.selectedDates = new BehaviorSubject<Set<Date>>(new Set());
     this.selectedDays = new BehaviorSubject(new Set());
   }
 
@@ -22,6 +23,28 @@ export class DateSelectioHandlerDirective implements OnInit, AfterViewChecked {
   }
 
   ngAfterViewChecked(): void {
-    console.table(this.matCalendar.monthView._matCalendarBody.activeCell);
+    for (let index in this.matCalendar.monthSelected) {
+
+    }
+    let HeaderElsClass = this.elRef.nativeElement.getElementsByClassName('mat-calendar-body-cell')
+    // let HeaderElsClass: MatCalendarCell<Date>[][] = this.matCalendar.monthView._matCalendarBody.rows;
+    let orderDate = Array.from(this.selectedDates.value);
+    // console.log(this.selectedDates.value);
+    // tempOrderDate is epoch array, i convert from epoch to date
+
+    for (let index in HeaderElsClass) {
+      if(typeof HeaderElsClass[index] === 'object') {
+        let headerClass = HeaderElsClass;
+
+        orderDate.find(each => {
+          console.log(each.getDate(), headerClass[index].children[0].textContent);
+          if (each.getDate() === headerClass[index].value) {
+            this.renderer.addClass(HeaderElsClass, 'mat-calendar-body-active');
+            this.renderer.setStyle(HeaderElsClass, 'font-weight', '900');
+          }
+          return false;
+        })
+      }
+    }
   }
 }
